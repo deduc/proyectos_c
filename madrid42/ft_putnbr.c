7f74d45309bb692c1c5606e26d2572db69a0ed89
PORTATIL-IVAN\deduc <deductivegoose@outlook.es>
2025-06-13 00:50:21 +0200

    ej 07

---
+++ madrid42/ft_putnbr.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putnbr(int nb);
+
+
+int main () {
+    int x = -780;
+    ft_putnbr(x);
+    return 0;
+}
+
+void ft_putnbr(int nb) {
+    if (nb < 0) {
+        char a = '-';
+        write(1, &a, 1);
+        nb = -nb;        
+    }
+
+    if (nb >= 10) {
+        int x = nb / 10;
+        ft_putnbr(x);
+    }
+
+    int c = nb % 10 + '0';
+    write(1, &c, 1);
+}
